d6adea0cddf62e819171977633e608bc207d8015
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-05-09 00:26:05 +0500

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,21 @@
+//  FOR of loop in Practice
+//  Q no 1
+// let friends:string[]=["Safdar","Ali","Sardar","Ali","Zafar","Ali"]
+// for(let i=0;i<friends.length;i++){
+//     console.log(` Well come to my youtube channel ${friends[0]}`);   
+// }
+// // Q no 2
+// let colors:string[] = ["Red","Yellow","Green","Pink","White"]
+// for( let i=0; i <=5; i++){
+//     console.log(`My Favourite Color is ${colors[2]}`);
+// }
+// //   Q no 3
+// let pakTeam:string[] =["BaberAzam","Rizwan","Fakher","Iftikhar","Shadab"]
+// for( let i=0; i <=3; i++){
+//     console.log(`${pakTeam[0]} is the captain of pakistan cricket team`);
+// }
+//  Q no 4
+var computers = ["DELL", "HP", "Apple", "TOSHIBA"];
+for (var i = 0; i <= 6; i++) {
+    console.log("I have ".concat(computers[0], " Laptop and ").concat(computers[1], " computer"));
+}
